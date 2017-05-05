b9082d67b599de0748fc82ddb437b42db59d6967
Andre Ramoni <ramoni@Portatrecos.local>
2017-05-05 16:31:47 -0300

    .

--- manifests/appbase/java/install.pp
+++ manifests/appbase/java/install.pp
@@ -1,4 +1,4 @@
 class rservers::appbase::java::install (
   $java_version = '1.8',
-  $java_source = 'openjdk',
+  $java_source  = 'openjdk',
 )
--- manifests/appbase/java/newrelic.pp
+++ manifests/appbase/java/newrelic.pp
@@ -1,4 +1,4 @@
 class rservers::appbase::java::newrelic (
   $install_newrelic_agent = false,
-  $newrelic_id = '',
+  $newrelic_id            = '',
 )
