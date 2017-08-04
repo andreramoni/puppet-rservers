a004841553ad7df9087d62be62d19b683a216c52
Andre Ramoni <ramoni@Andres-iMac.local>
2017-08-03 22:09:44 -0300

    .

--- manifests/loadbalance/params.pp
+++ manifests/loadbalance/params.pp
@@ -2,7 +2,7 @@ class rservers::loadbalance::params {
   if $::fqdn =~ /^centos\.localdomain$/ {
       $vr_rid_prod = '100'
       $vips = [
-               { ip => '10.49.1.226/21', label => "${facts['networking']['primary']}:0" },
+               { ip => '10.10.10.100/24', label => "${facts['networking']['primary']}:0" },
               ]
   }
 
