0cf5adc0280f7ce41d85332d5202fe51e46b9cc0
Andre Ramoni <andre.ramoni@gmail.com>
2018-04-25 21:38:59 -0300

    .

--- manifests/firewall/params.pp
+++ manifests/firewall/params.pp
@@ -3,7 +3,7 @@ class rservers::firewall::params {
   # Firewalls fw0[12].ext.lab
   if $::fqdn =~ /^fw0(1|2)\.ext\.lab$/ {
     $vr_id = '100'
-    $vips_ext = [ { ip => '192.168.12.20/24', label => "${::iface_ext}:0" }, ],
-    $vips_mgmt = [ { ip => '10.255.0.250/16', label => "${::iface_mgmt}:0" }, ],
+    $vips_ext = [ { ip => '192.168.12.20/24', label => "${::iface_ext}:0" }, ]
+    $vips_mgmt = [ { ip => '10.255.0.250/16', label => "${::iface_mgmt}:0" }, ]
   }
 }
