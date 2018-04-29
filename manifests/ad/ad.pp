503b139e2e17c26f5aecb7222ca256c677a616c4
Andre Ramoni <andre.ramoni@gmail.com>
2018-04-29 11:59:05 -0300

    .

--- manifests/ad/ad.pp
+++ manifests/ad/ad.pp
@@ -1,6 +1,5 @@
 class rservers::ad::ad {
-  $ad_features = ['Active Directory Domain Services' ] 
-  windowsfeature { $ad_features:
+  windowsfeature { 'Active\ Directory\ Domain\ Services':
     ensure => 'present',
 #    installmanagementtools => true,
   }  
