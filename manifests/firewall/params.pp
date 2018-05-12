6ecf0c107951dfac45d426744fcef2e25f27e7d4
Andre Ramoni <andre.ramoni@gmail.com>
2018-05-11 23:11:26 -0300

    .

--- manifests/firewall/params.pp
+++ manifests/firewall/params.pp
@@ -6,7 +6,9 @@ class rservers::firewall::params {
     $vips_ext  = [ { ip => '192.168.12.254/24', label => "${::iface_ext}:0" }, ]
     $vr_id_srv = '101'
     $vips_srv = [ { ip => '10.0.0.1/16', label => "${::iface_srv}:0" }, ]
-    $vr_id_dmz1 = '102'
-    $vips_dmz1 = [ { ip => '10.1.0.1/16', label => "${::iface_dmz1}:0" }, ]
+    $vr_id_dmz = '102'
+    $vips_dmz = [ { ip => '10.1.0.1/16', label => "${::iface_dmz}:0" }, ]
+    $vr_id_mgmt = '103'
+    $vips_mgmt = [ { ip => '10.255.0.1/16', label => "${::iface_mgmt}:0" }, ]
   }
 }
