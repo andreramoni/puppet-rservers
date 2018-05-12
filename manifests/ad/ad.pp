9d19a022f8879e0b885b17f8ebbcdc137dbaf177
Andre Ramoni <andre.ramoni@gmail.com>
2018-05-12 19:57:50 -0300

    .

--- manifests/ad/ad.pp
+++ manifests/ad/ad.pp
@@ -1,26 +1,6 @@
 class rservers::ad::ad {
-#  windowsfeature { 'ad-domain-services':
-#    ensure => 'present',
-#    installmanagementtools => true,
-#  }  
- class {'windows_ad':
-      install                => present,
-      installmanagementtools => true,
-      restart                => true,
-      installflag            => true,
-      configure              => present,
-      configureflag          => true,
-      domain                 => 'forest',
-      domainname             => 'jre.local',
-      netbiosdomainname      => 'jre',
-      domainlevel            => '6',
-      forestlevel            => '6',
-      databasepath           => 'c:\\windows\\ntds',
-      logpath                => 'c:\\windows\\ntds',
-      sysvolpath             => 'c:\\windows\\sysvol',
-      installtype            => 'domain',
-      dsrmpassword           => 'password',
-      installdns             => 'yes',
-      localadminpassword     => 'password',
-    }
+  windowsfeature { 'ad-domain-services':
+    ensure => 'present',
+    installmanagementtools => true,
+  }  
 }
