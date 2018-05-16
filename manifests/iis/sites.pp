62798e178c1f20247a1a5c2bef5251834d8c77de
Andre Ramoni <andre.ramoni@gmail.com>
2018-05-16 20:47:58 -0300

    .

--- manifests/iis/sites.pp
+++ manifests/iis/sites.pp
@@ -1,3 +1,12 @@
 class rservers::iis::sites {
-
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultAppPool',
+    require         => File['minimal'],
+  }
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
+  }
 }
