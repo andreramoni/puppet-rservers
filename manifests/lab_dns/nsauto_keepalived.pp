dd3c893ffa59f5a99412744e123d2ebaf59f36f8
Andre Ramoni <andre.ramoni@gmail.com>
2018-06-15 22:41:00 -0300

    .

--- manifests/lab_dns/nsauto_keepalived.pp
+++ manifests/lab_dns/nsauto_keepalived.pp
@@ -2,6 +2,11 @@ class rservers::lab_dns::nsauto_keepalived {
   package { 'keepalived':
     ensure => present,
   }
+  service { 'keepalived':
+    ensure => running,
+    enable => true,
+    require => Package[keepalived],
+  }
   file { '/etc/keepalived/keepalived.conf':
     content => template('rservers/lab_dns/nsauto-keepalived.conf.erb'),
     notify  => Service[keepalived],
