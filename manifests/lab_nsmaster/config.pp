d2a8133a19095a906b8f8e4bcbc97d691392361d
Andre Ramoni <andre.ramoni@gmail.com>
2018-06-15 21:07:09 -0300

    .

--- manifests/lab_nsmaster/config.pp
+++ manifests/lab_nsmaster/config.pp
@@ -1,5 +1,13 @@
-class rservers::lab_nsmaster::package {
-  package { 'bind':
-    ensure => present,
+class rservers::lab_nsmaster::config {
+  File { 
+    ensure  => file,
+    owner   => 'root',
+    group   => 'named',
+    mode    => '0640',
+    source  => 'puppet:///modules/rservers/lab_nsmaster/named.conf',
+    require => Package[bind],
+    notify  => Service[named],
   }
+  file { '/etc/named.conf': source  => 'puppet:///modules/rservers/lab_nsmaster/named.conf' }
+  file { '/etc/named/zones.conf': source  => 'puppet:///modules/rservers/lab_nsmaster/zones.conf' }
 }
