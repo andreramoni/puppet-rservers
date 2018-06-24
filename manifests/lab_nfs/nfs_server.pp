8452ae4cdfb86e83186562ac7ac651f7ca48045b
Andre Ramoni <andre.ramoni@gmail.com>
2018-06-24 11:18:32 -0300

    .

---
+++ manifests/lab_nfs/nfs_server.pp
@@ -0,0 +1,15 @@
+class rservers::lab_nfs::nfs_server {
+  service { 'nfs':
+    ensure => running,
+    enable => true,
+    require => Package[nfs-utils],
+  }
+  file { '/etc/exports':
+    ensure  => file,
+    content => '/data *(rw,no_root_squash)',
+    notify  => Service[nfs],
+  }
+  package { 'nfs-utils':
+    ensure => installed,
+  }
+}
