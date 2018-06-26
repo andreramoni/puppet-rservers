6c527b9b2a41f2e9c362e96b9cc6a564a7c90564
Andre Ramoni <andre.ramoni@gmail.com>
2018-06-25 22:09:44 -0300

    .

--- manifests/testing.pp
+++ manifests/testing.pp
@@ -3,6 +3,21 @@ class rservers::testing {
     manage_repo => true,
     features    => ['checker', 'mainlog', 'command'],
   }
-  
+  include ::mysql::server
+
+  mysql::db { 'icinga2':
+    user     => 'icinga2',
+    password => 'icinga2',
+    host     => '127.0.0.1',
+    grant    => ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE VIEW', 'CREATE', 'INDEX', 'EXECUTE', 'ALTER'],
+  }
+
+  class{ '::icinga2::feature::idomysql':
+    user          => 'icinga2',
+    password      => 'icinga2',
+    database      => 'icinga2',
+    import_schema => true,
+    require       => Mysql::Db['icinga2'],
+  }  
 }
 
