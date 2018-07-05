d9d69b8e2bead5260a3c2b3164852af733947468
Andre Ramoni <andre.ramoni@gmail.com>
2018-07-04 23:13:59 -0300

    .

--- manifests/jblin/packages.pp
+++ manifests/jblin/packages.pp
@@ -6,7 +6,7 @@ class rservers::jblin::packages {
 user { 'admin':
   ensure           => 'present',
   groups           => ['wheel','users'],
-  manage_home      => true,
+  managehome       => true,
   home             => '/home/admin',
   password         => '$5$WcnT3Zb2$RS.VJHPUBPw4r5nbGqnPEqvsq3GJsBSZwFdxt7GVow0',
   password_max_age => '99999',
