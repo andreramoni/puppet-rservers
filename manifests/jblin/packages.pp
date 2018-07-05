f3719b65e145ba047fa8ce976b2a6221f4030c65
Andre Ramoni <andre.ramoni@gmail.com>
2018-07-04 21:52:01 -0300

    .

--- manifests/jblin/packages.pp
+++ manifests/jblin/packages.pp
@@ -1,5 +1,9 @@
 class rservers::jblin::packages {
-  yum::group { 'MATE Desktop':
+  yum::group { 'Server with GUI':
+    ensure => present,
+    timeout => 1800,
+  }
+  yum::group { 'Cinnamon Desktop':
     ensure => present,
     timeout => 1800,
   }
