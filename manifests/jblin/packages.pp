7e6756d683304f082c2ac9493c4b275765845767
Andre Ramoni <andre.ramoni@gmail.com>
2018-07-04 23:12:39 -0300

    .

--- manifests/jblin/packages.pp
+++ manifests/jblin/packages.pp
@@ -5,13 +5,12 @@ class rservers::jblin::packages {
   }
 user { 'admin':
   ensure           => 'present',
-  gid              => '100',
-  groups           => ['wheel'],
+  groups           => ['wheel','users'],
+  manage_home      => true,
   home             => '/home/admin',
   password         => '$5$WcnT3Zb2$RS.VJHPUBPw4r5nbGqnPEqvsq3GJsBSZwFdxt7GVow0',
   password_max_age => '99999',
   password_min_age => '0',
   shell            => '/bin/bash',
-  uid              => '1000',
 }
 }
