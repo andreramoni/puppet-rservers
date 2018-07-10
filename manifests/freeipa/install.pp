9b32d1171cea181ee3e40e1817ba7ab5e1ed952c
Andre Ramoni <andre.ramoni@gmail.com>
2018-07-09 21:34:47 -0300

    Fixed boolean

--- manifests/freeipa/install.pp
+++ manifests/freeipa/install.pp
@@ -4,7 +4,7 @@ class rservers::freeipa::install {
     notify => Exec['freeipa initial install'],
   }
   exec { 'freeipa initial install':
-    refreshonly => yes,
+    refreshonly => true,
     command     => "/usr/sbin/ipa-server-install -U -p 'adminadmin' -a 'adminadmin' -r ${::domain}",
     timeout     => 1200,
   }
