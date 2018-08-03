0e9a049905be6ee6ebae23a54a03cdfe83a391fc
Andre Ramoni <andre.ramoni@gmail.com>
2018-08-03 21:17:49 +0000

    .

--- manifests/app01srv/files.pp
+++ manifests/app01srv/files.pp
@@ -1,7 +1,7 @@
 class rservers::app01srv::files {
   file { 'app01':
     ensure  => directory,
-    path    => [ 'c:\\inetpub', 'c:\\inetpub\\app01' ],
+    path    => 'c:\\inetpub\\app01',
     recurse => true,
     source  => 'puppet:///modules/rservers/app01',
     require => Iis_site['app01'],
