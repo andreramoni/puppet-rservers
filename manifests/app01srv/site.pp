254bd3c86ef866d40549d7c20d5e531a8679a500
Andre Ramoni <andre.ramoni@gmail.com>
2018-08-03 20:50:04 +0000

    .

--- manifests/app01srv/site.pp
+++ manifests/app01srv/site.pp
@@ -1,9 +1,4 @@
 class rservers::app01srv::site {
-
-  iis_site { 'Default Web Site':
-    ensure => 'absent',
-  }
-
   iis_site { 'app01':
     ensure          => 'started',
     physicalpath    => 'c:\\inetpub\\app01',
