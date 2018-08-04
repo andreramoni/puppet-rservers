9940481cfa7bb5f218881567514b9c9c22ef210c
Andre Ramoni <andre.ramoni@gmail.com>
2018-08-03 22:37:34 -0300

    .

--- manifests/app01haproxy/config.pp
+++ manifests/app01haproxy/config.pp
@@ -1,4 +1,12 @@
 class rservers::app01haproxy::config {
+  $query = foreman({foreman_user => 'readonly',
+                  foreman_pass => 'readonly',
+                  item         => 'hosts',
+                  search       => 'hostgroup_fullname ~ Windows/App01',
+  })
+
+  $nodes = $query['results']
+
   file { '/etc/haproxy/haproxy.cfg':
     ensure  => file,
     content => template('rservers/app01/app01haproxy/haproxy.cfg.erb'),
