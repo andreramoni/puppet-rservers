12baf50ce8029773a17947a316881658edee75dd
Andre Ramoni <andre.ramoni@gmail.com>
2018-08-04 14:09:11 -0300

    .

--- manifests/nginx/config.pp
+++
@@ -1,9 +0,0 @@
-class rservers::nginx::config {
-  file { '/etc/nginx':
-    ensure  => directory,
-    recurse => true,
-    source  => 'puppet:///modules/rservers/nginx/config',
-    require => Package['nginx'],
-  }
-}
-
