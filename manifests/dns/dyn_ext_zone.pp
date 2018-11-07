2a802c23aa557362b44a2110f19ab13f41160cb6
Andre Ramoni <andre.ramoni@gmail.com>
2018-11-07 18:46:41 -0200

    .

---
+++ manifests/dns/dyn_ext_zone.pp
@@ -0,0 +1,10 @@
+class rservers::dns::dyn_ext_zone {
+  file { '/var/named/zones/ext.dc1.lab.zone.dyn':
+    ensure  => file,
+    content => template('templates/dns/ext.dc1.lab.zone.dyn.erb'),
+    owner   => 'named',
+    group   => 'named',
+    require => Class['rservers::dns::zones'],
+    notify  => Service[named],
+  }
+}
