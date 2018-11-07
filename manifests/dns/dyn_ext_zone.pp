44ef623a180ed016335d78df06b12fece5168505
Andre Ramoni <andre.ramoni@gmail.com>
2018-11-07 19:01:01 -0200

    .

--- manifests/dns/dyn_ext_zone.pp
+++ manifests/dns/dyn_ext_zone.pp
@@ -1,4 +1,15 @@
 class rservers::dns::dyn_ext_zone {
+
+  $query = foreman({foreman_user => 'readonly',
+                  foreman_pass => 'readonly',
+                  item         => 'hosts',
+                  search       => 'domain = ext.dc1.lab',
+                  filter_result => [ 'ip' ] ,
+  })
+
+  $nodes = $query
+
+
   file { '/var/named/zones/ext.dc1.lab.zone.dyn':
     ensure  => file,
     content => template('templates/dns/ext.dc1.lab.zone.dyn.erb'),
